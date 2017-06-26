f7e97518664d1c01e5a64e75695de7916aa4b2be
jacob ruleaux and Michelle Poterek <jacob@>
2017-06-26 09:13:11 -0700

    build vars and foreach loop

--- index.php
+++ index.php
@@ -1,11 +1,34 @@
 <?php
-function _construct($make_model, $price, $image_path, $miles);
+
+    class Car
+    {
+        public $make_model;
+        public $price;
+        public $image_path;
+        public $miles;
+    }
+
+function __construct($make_model, $price, $image_path, $miles)
 {
       $this->model = $make_model;
       $this->price = $price;
       $this->image = $image_path;
       $this->miles = $miles;
 }
+      $first_car = new Car("accord", "$700", "image", "165,098");
+      $second_car = new Car("volvo", "$1200", "image", "198,345");
+      $third_car = new Car("vw", "$350", "image", "199,097");
+
+$cars = array($porsche, $ford, $lexus, $mercedes);
+
+$cars_matching_search = array();
+foreach ($cars as $car) {
+if ($car->price < $_GET["price"]) {
+array_push($cars_matching_search, $car);
+}
+   
+
+
 ?>
 
 
@@ -24,6 +47,7 @@ function _construct($make_model, $price, $image_path, $miles);
                 echo "<li> $$car->price </li>";
                 echo "<li> Miles: $car->miles </li>";
             echo "</ul>";
+          }
     ?>
     </div>
 </body>
