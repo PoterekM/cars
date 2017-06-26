28a20a0cdfc485c91a0c7ef5caf9f81c25d78800
jacob ruleaux and Michelle Poterek <jacob@>
2017-06-26 10:17:51 -0700

    add images to file

--- index.php
+++ index.php
@@ -2,33 +2,32 @@
 
     class Car
     {
-        public $make_model;
-        public $price;
-        public $image_path;
-        public $miles;
+    public $make_model;
+    public $price;
+    public $image_path;
+    public $miles;
+
+
+    function __construct($make_model, $price, $image_path, $miles)
+    {
+        $this->model = $make_model;
+        $this->price = $price;
+        $this->image = $image_path;
+        $this->miles = $miles;
     }
-
-function __construct($make_model, $price, $image_path, $miles)
-{
-      $this->model = $make_model;
-      $this->price = $price;
-      $this->image = $image_path;
-      $this->miles = $miles;
 }
-      $first_car = new Car("accord", "$700", "image", "165,098");
-      $second_car = new Car("volvo", "$1200", "image", "198,345");
-      $third_car = new Car("vw", "$350", "image", "199,097");
+    $first_car = new Car("accord", 700, "img/accord.jpeg", 165098);
+    $second_car = new Car("volvo", 1200, "img/volvo.jpeg", 198345);
+    $third_car = new Car("vw", 350, "img/vw_büs.jpg", 199097);
 
-$cars = array($porsche, $ford, $lexus, $mercedes);
+$cars = array($first_car, $second_car, $third_car);
 
 $cars_matching_search = array();
-foreach ($cars as $car) {
-if ($car->price < $_GET["price"]) {
-array_push($cars_matching_search, $car);
+    foreach ($cars as $car) {
+        if ($car->price < $_GET["price"]) {
+        array_push($cars_matching_search, $car);
+        }
 }
-   
-
-
 ?>
 
 
@@ -42,7 +41,7 @@ array_push($cars_matching_search, $car);
     <div class="container">
     <?php
         foreach ($cars_matching_search as $car) {
-            echo "<li> $car->make_model </li>";
+            echo "<li> $car->model </li>";
             echo "<ul>";
                 echo "<li> $$car->price </li>";
                 echo "<li> Miles: $car->miles </li>";
